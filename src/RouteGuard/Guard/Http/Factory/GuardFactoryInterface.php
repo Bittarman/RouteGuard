e0b7f6baa4ae2897cdfd91a0d99928aee1b34bce
Peter Aba <devmonk@devmonk.com>
2014-01-30 02:14:55 +0100

    Adding guard factory interface

---
+++ src/RouteGuard/Guard/Http/Factory/GuardFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * RouteGuard for Zend Framework 2
+ *
+ * @link https://github.com/Bittarman/RouteGuard for the canonical source repository
+ * @copyright Copyright (c) Ryan Mauger 2013 (http://rmauger.co.uk)
+ * @license https://github.com/Bittarman/RouteGuard/blob/master/LICENSE
+ */
+
+namespace RouteGuard\Guard\Http\Factory;
+
+interface GuardFactoryInterface
+{
+    public function setCreationOptions($options);
+}
